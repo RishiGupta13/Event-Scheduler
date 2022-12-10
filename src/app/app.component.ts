710b15b1890752c7aadbdd1e62f455298257a998
RishiGupta13 <rishi131100@gmail.com>
2022-12-10 23:04:08 +0530

    Data binding Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,61 @@
-import { Component } from '@angular/core';
-import {View,EventSettingsModel} from '@syncfusion/ej2-angular-schedule';
+import { Component, ViewEncapsulation, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import {
+  View, EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService, ResizeService,
+  DragAndDropService, NavigatingEventArgs, ActionEventArgs, ScheduleComponent
+} from '@syncfusion/ej2-angular-schedule';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { scheduleData } from '../data';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'app-root',
-  template:'<ejs-schedule [eventSettings]="eventObject" [selectedDate]="setDate"></ejs-schedule>',
-  // templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  providers: [DayService, WeekService, WorkWeekService, MonthService, ResizeService, DragAndDropService],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'Event-Scheduler';
-  public setDate:Date=new Date(2023,3,15);
-  // public setView:View='Workweek';
-  public eventObject:EventSettingsModel={
-    dataSource:[{
-      Subject:"Study React",
-      StartTime:new Date(2023,0,4,0),
-      EndTime:new Date(2023,0,10,6,0),
-      IsReadonly:true,
-      // IsBlock:true  : this will block the cell and will not let us add anything
-      // RecurrenceRule:"FREQ-DAILY;INTERVAL=1;COUNT=10"
-    }],
-    // fields:{
-    //   subject:{name:'EventTitle'},
-    //   startTime:{name:'EventStart'},
-    //   endTime:{name:'EventEnd'}
-    // }
+  @ViewChild('scheduleObj')
+  public scheduleObj!: ScheduleComponent;
+
+  public data: Record<string, any>[] = scheduleData;
+  public selectedDate: Date = new Date(2021, 0, 10);
+  public eventSettings: EventSettingsModel = { dataSource: this.data };
+  public currentView: View = 'Week';
+  public isHubConnected = false;
+  public connection!: HubConnection;
+
+  public ngOnInit(): void {
+   
+
+    const url = 'https://ej2.syncfusion.com/aspnetcore/scheduleHub/';
+    this.connection = new HubConnectionBuilder().withUrl(url, { withCredentials: false }).withAutomaticReconnect().build();
+    this.connection.on('ReceiveData', (action: string, data: View | Record<string, any>[]) => {
+      if (action === 'view') {
+        this.scheduleObj.currentView = data as View;
+      }
+      if (action === 'eventCreated' || action === 'eventChanged' || action === 'eventRemoved') {
+        this.scheduleObj.eventSettings.dataSource = data as Record<string, any>[];
+      }
+    });
+
+    this.connection.start().then(() => { this.isHubConnected = true; }).catch(() => { this.isHubConnected = false; });
   }
 
+  public onNavigating(args: NavigatingEventArgs): void {
+    if (args.action === 'view' && this.isHubConnected) {
+      this.connection.invoke('SendData', args.action, args.currentView);
+    }
+  }
+
+  public onActionComplete(args: ActionEventArgs): void {
+    if (this.isHubConnected && (args.requestType === 'eventCreated' || args.requestType === 'eventChanged' || args.requestType === 'eventRemoved')) {
+      this.connection.invoke('SendData', args.requestType, this.scheduleObj.eventSettings.dataSource);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    if (this.connection) {
+      this.connection.stop().then(() => { this.isHubConnected = false; }).catch((err) => { console.log(err); });
+    }
+  }
 }
